fcbe1cad5d9ee8518317cf7bfe270609fb45ff54
NMGVox <nijeemgreene@gmail.com>
2023-03-14 16:05:59 -0400

    Add password matcing validation via JavaScript
    Wit this commit, all fundamental and bonus features needed for this
    project are complete.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function validate(){
-    console.log("Hey man.");
-    return true;
+function validate(e){
+    let password = document.querySelector("#pw");
+    let warning = document.querySelector(".pass_warning");
+    console.log(confirm_password.value);
+    if (password.value === confirm_password.value){
+        confirm_password.setCustomValidity("");
+        warning.style.visibility='hidden';
+        return;
+    }
+    confirm_password.setCustomValidity("Passwords do not match!");
+    warning.style.visibility='visible';
+    return;
 }
+
+let confirm_password = document.querySelector("#confirm_pw");
+confirm_password.addEventListener('input', validate);
